79bce57de1bd7bfc5c2425ad5556a17a369796a6
gustavo4872 <gustavo4872@gmail.com>
2019-03-08 21:16:29 -0300

    Rest Categoria

---
+++ src/main/java/br/unisul/web/sexta/resources/CategoriaResource.java
@@ -0,0 +1,24 @@
+package br.unisul.web.sexta.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.unisul.web.sexta.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar (){
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(new Categoria(1, "Informática"));
+		lista.add(new Categoria(2, "Camping"));
+		return lista;
+	}
+	
+}
